650b4c124766276b65ab589efe41ea7a3920597d
lesnoy <bigib@yandex.ru>
2024-03-11 19:32:31 +0300

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,8 @@ public class Main {
         post.patronymic = "Иванович";
         post.birthday = new FormDate();
         post.birthday.day = 13;
-        post.birthday.month = 6;
-        post.birthday.year = 1999;
+        post.birthday.month = 12;
+        post.birthday.year = 2000;
         post.passport = "4444 № 44444444";
         post.phone = "+7 (999)-999-99-99";
         post.subscription = true;
